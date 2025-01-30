7359ce14c83de8285b056e9868fe45a014ff78e3
keuntungankamboja <keuntungankamboja@gmail.com>
2025-01-30 16:16:01 +0700

    upload file

---
+++ sinline/application/views/import/foother.php
@@ -0,0 +1,3 @@
+<div class="foother text-center ">
+    <p class="pull-right">Repost by <a href="https://stokcoding.com/" title="StokCoding.com" target="_blank">StokCoding.com</a> | <a href="#">Ke Atas</a></p>
+</div>
